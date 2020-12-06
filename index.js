563293518ee67e2f2bdf08e33c8a70b6fb0ce767
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-06 21:24:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function sayHiToGrandma(string){
     return "YES INDEED!"
   }
   if (lowercase){
-    return "I can't hear you!"
+    return "I can\'t hear you!"
   }
   if (string= "I love you, Grandma."){
     return "I love you, too."
